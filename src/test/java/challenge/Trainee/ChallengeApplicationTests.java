826d0862e879ffa6eab64bd2867d60d2359ead67
Costa-Julian <costa-julian@outlook.com>
2020-08-11 11:19:18 -0300

    esta bien

---
+++ src/test/java/challenge/Trainee/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.Trainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
